06259552543eb42fac9891ae2c62772215d067dc
Alexander Kondratskiy <kholdstare0.0@gmail.com>
2014-06-05 19:29:52 -0400

    Splitting off ChunkedQueue

--- src/Control/Concurrent/STM/TChunkedQueue.hs
+++ src/Control/Concurrent/STM/TChunkedQueue.hs
@@ -31,42 +31,12 @@ import Control.Concurrent.STM.TVar
 import Control.Concurrent.Async (race)
 import Control.Concurrent (threadDelay)
 
-import Data.Monoid
+import Control.Concurrent.STM.ChunkedQueue
 
 ----------------------------------------------------------------
 
--- TODO: make closeable?
 data TChunkedQueue a = TChunkedQueue
     {-# UNPACK #-} !(TVar (ChunkedQueue a))
-    
------------------
-data ChunkedQueue a = ChunkedQueue [Chunk a]
-
-instance Monoid (ChunkedQueue a) where
-    mempty = ChunkedQueue []
-    (ChunkedQueue []) `mappend` b = b
-    (ChunkedQueue a) `mappend` (ChunkedQueue b) = ChunkedQueue (b ++ a)
-
-consumeQueue :: ChunkedQueue a -> [a]
-consumeQueue (ChunkedQueue chunks) = go [] chunks
-    where
-        go acc [] = acc
-        go acc (Forward x : xs) = go (x ++ acc) xs
-        go acc (Reverse x : xs) = go ((reverse x) ++ acc) xs
-
-enqueueOne :: ChunkedQueue a -> a -> ChunkedQueue a
-enqueueOne (ChunkedQueue (Reverse xs:chunks)) x =
-    ChunkedQueue (Reverse (x:xs):chunks)
-enqueueOne (ChunkedQueue chunks) x =
-    ChunkedQueue (Reverse [x]:chunks)
-
-
-enqueueMany :: ChunkedQueue a -> [a] -> ChunkedQueue a
-enqueueMany chQueue [x] = enqueueOne chQueue x
-enqueueMany (ChunkedQueue chunks) xs = ChunkedQueue (Forward xs:chunks)
-
-
-data Chunk a = Forward [a] | Reverse [a]
 
 
 -- | Build and returns a new instance of @TChunkedQueue@
