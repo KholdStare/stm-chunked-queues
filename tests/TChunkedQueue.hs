cc8c5e7e6b77c155420b24535d9bc69b5e0de638
Alexander Kondratskiy <kholdstare0.0@gmail.com>
2014-06-05 09:12:25 -0400

    Adding more tests

--- tests/TChunkedQueue.hs
+++ tests/TChunkedQueue.hs
@@ -3,7 +3,7 @@ module TChunkedQueue (
     tests
 ) where
 
-{-import Control.Applicative ((<$>))-}
+import Control.Applicative ((<$>))
 import Control.Monad
 import Control.Monad.STM   (atomically)
 import Control.Concurrent.STM.TVar
@@ -31,8 +31,14 @@ consumingThread pullFunc areWeDone queue = go []
 
             if done
             then do
-                remainingItems <- atomically $ tryDrainTChunkedQueue queue
-                return $ reverse (consumeQueue remainingItems : allItems)
+                remainingItems <- consumeQueue <$> atomically (tryDrainTChunkedQueue queue)
+                let result = reverse $
+                        if null remainingItems 
+                            then allItems
+                            else remainingItems : allItems
+                    
+                return result
+
             else go allItems
 
 
@@ -53,7 +59,7 @@ runQueueActions actions = do
     let dumpToQueue = 
             mapM_ (atomically . writeTChunkedQueue queue) 
 
-    forM_ actions $ \action -> do
+    forM_ actions $ \action ->
         case action of
             Enqueue items -> dumpToQueue items
             Wait delay -> threadDelay delay
@@ -69,10 +75,45 @@ testActions :: (Eq a, Show a)
             -> Assertion
 testActions actions expectations = do
     results <- runQueueActions actions
-    assertEqual "items dequeued have to be correct" results expectations
+    assertEqual "items dequeued have to be grouped correctly" expectations results
+
+-- | Convenience specialization to avoid "Default Constraint" warnings
+testActions' :: [QueueAction Int] -> [[Int]] -> Assertion
+testActions' = testActions
+
 
 tests :: [TestTree]
-tests = [ testCase "Simple" (testActions [Enqueue [0]] [[0],[]]) ]
-{-tests = [ testProperty "No refraction if index unchanged" prop_sameIndex-}
-        {-, testProperty "Critical angle in glass" $ forAll (choose (0, glassCriticalAngle))-}
-                        {-$ checkTotalInternalReflection glassRefractiveIndex ]-}
+tests = 
+    let shortPause = 10000
+        longPause  = 60000
+    in
+
+    [ testCase "Simple" $ testActions' [Enqueue [0]] $ expect [[0]]
+    , testCase "Two" $ testActions' [
+            Enqueue [0],
+            Wait shortPause,
+            Enqueue [1]]
+            $ expect [[0, 1]]
+    , testCase "Two With Pause" $ testActions' [
+            Enqueue [0],
+            Wait longPause,
+            Enqueue [1]]
+            $ expect [[0], [1]]
+    , testCase "Two Lists" $ testActions' [
+            Enqueue [0..10],
+            Wait shortPause,
+            Enqueue [11..20]]
+            $ expect [[0..20]]
+    , testCase "Many With Pauses" $ testActions' [
+            Enqueue [0..10], Wait shortPause, Enqueue [11..20],
+            Wait longPause,
+            Enqueue [21..30],
+            Wait longPause,
+            Enqueue [31..40], Wait shortPause, Enqueue [41..50],
+            Wait longPause,
+            Enqueue [51..60] ]
+            $ expect [[0..20], [21..30], [31..50], [51..60]]
+    ]
+
+    where
+        expect = id
