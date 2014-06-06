ca12e601d73c76d883b5aa4333558283ee1290e5
Alexander Kondratskiy <kholdstare0.0@gmail.com>
2014-06-06 18:01:25 -0400

    Adding simple tests to TMChunkedQueue

--- tests/UnitTests.hs
+++ tests/UnitTests.hs
@@ -2,8 +2,11 @@ module Main (main) where
 
 import Test.Tasty
 import qualified TChunkedQueue
+import qualified TMChunkedQueue
 
 main :: IO ()
 main = defaultMain $
-    testGroup "STM TChunkedQueues"
-        [ testGroup "TChunkedQueue" TChunkedQueue.tests ]
+    testGroup "STM ChunkedQueues"
+        [ testGroup "TChunkedQueue" TChunkedQueue.tests
+        , testGroup "TMChunkedQueue" TMChunkedQueue.tests
+        ]
